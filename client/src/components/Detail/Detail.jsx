330df82667d41eadbed464a303c395a8895716a2
Santiago Sevilla <63272703+santisevilla@users.noreply.github.com>
2022-05-05 17:52:54 -0300

    cambios

--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -65,7 +65,7 @@ export default function Detail() {
         </div>
         <div className="buttonDetail">
           <Link to="/home">
-            <button>BACK</button>
+            <button>X</button>
           </Link>
         </div>
       </div>
--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { 
-  getActivities, 
+import {
+  getActivities,
   getCountries,
   orderByName,
   orderByPopulation,
   filterByActivity,
   filterByContinent,
-  filterByPopulation
+  filterByPopulation,
 } from "../../actions";
 import Paginado from "../Paginado/Paginado";
 import Card from "../Card/Card";
@@ -47,31 +47,31 @@ export default function Home() {
     e.preventDefault();
     dispatch(getCountries());
   }
-  
+
   function handleAlphabeticalOrder(e) {
     e.preventDefault();
     dispatch(orderByName(e.target.value));
-    setCurrentPage(1)
-    setOrder(`Ordenado ${e.target.value}`)
+    setCurrentPage(1);
+    setOrder(`Ordenado ${e.target.value}`);
   }
 
   function handleOrderPopulation(e) {
     e.preventDefault();
     dispatch(orderByPopulation(e.target.value));
-    setCurrentPage(1)
-    setOrder(`Ordenado ${e.target.value}`)
+    setCurrentPage(1);
+    setOrder(`Ordenado ${e.target.value}`);
   }
 
   function handleFilterContinent(e) {
     e.preventDefault();
     dispatch(filterByContinent(e.target.value));
-    setOrder(`Ordenado ${e.target.value}`)
+    setOrder(`Ordenado ${e.target.value}`);
   }
 
   function handleFilterActivity(e) {
     e.preventDefault();
     dispatch(filterByActivity(e.target.value));
-    setOrder(`Ordenado ${e.target.value}`)
+    setOrder(`Ordenado ${e.target.value}`);
   }
 
   // function handleFilterPopulation(e) {
@@ -85,78 +85,101 @@ export default function Home() {
   return (
     <div className="bodyHome">
       <div className="bodyNavBar">
-      <div className="divActivity">
-        <Link to="/activities">
-          Create a new activity!
-        </Link>
-      </div>
-      <div className="div">
-        <button onClick={(e) => {handleClick(e)}}>
-          All Countries
-        </button>
-      </div>
-      <div className="div">
-        <SearchBar />  
-      </div>
-      {/* <div>
+        <div className="divActivity">
+          <Link
+            to="/activities"
+            style={{ textDecoration: "none", color: "white" }}
+          >
+            Create a new activity!
+          </Link>
+        </div>
+        <div className="div">
+          <button
+            className="buttonAll"
+            onClick={(e) => {
+              handleClick(e);
+            }}
+          >
+            All Countries
+          </button>
+        </div>
+        <div className="div">
+          <SearchBar />
+        </div>
+        {/* <div>
         <label> Population </label>
         <select onChange={(e) => handleFilterPopulation(e)}>
           <option value="All"></option>
           <option value="menor">menor a 50000</option>
         </select>
       </div> */}
-      <div className="div">
-        <label> Order: </label>
-        <select onChange={(e) => handleAlphabeticalOrder(e)}>
-          <option value="All"></option>
-          <option value="asc">A-Z</option>
-          <option value="desc">Z-A</option>
-        </select>
-      </div>
-      <div className="div">
-        <label> Population: </label>
-        <select onChange={(e) => handleOrderPopulation(e)}>
-          <option value="All"></option>
-          <option value="asc"> Highest </option>
-          <option value="desc"> Lowest </option>
-        </select>
-      </div>  
-      <div className="div">
-        <label> Continents: </label>
-        <select onChange={(e) => handleFilterContinent(e)}>
-          <option value="All"></option>
-          <option value="Africa">Africa</option>
-          <option value="Antarctica">Antarctica</option>
-          <option value="Asia">Asia</option>
-          <option value="Europe">Europa</option>
-          <option value="North America">North America</option>
-          <option value="Oceania">Oceania</option>
-          <option value="South America">South America</option>
-        </select>
-      </div>
-      <div className="div">
-        <label> Activity: </label>
-        <select onChange={(e) => handleFilterActivity(e)}>
-          <option value="All"></option>
-          {allActivities.map((element) => (
-            <option value={element.name} key={element}>
-              {element.name}
-            </option>
-          ))}
-        </select>
-      </div>
+        <div className="div">
+          <label> Order: </label>
+          <select
+            className="selectStyle"
+            onChange={(e) => handleAlphabeticalOrder(e)}
+          >
+            <option value="All"></option>
+            <option value="asc">A-Z</option>
+            <option value="desc">Z-A</option>
+          </select>
+        </div>
+        <div className="div">
+          <label> Population: </label>
+          <select
+            className="selectStyle"
+            onChange={(e) => handleOrderPopulation(e)}
+          >
+            <option value="All"></option>
+            <option value="asc"> Highest </option>
+            <option value="desc"> Lowest </option>
+          </select>
+        </div>
+        <div className="div">
+          <label> Continents: </label>
+          <select
+            className="selectStyle"
+            onChange={(e) => handleFilterContinent(e)}
+          >
+            <option value="All"></option>
+            <option value="Africa">Africa</option>
+            <option value="Antarctica">Antarctica</option>
+            <option value="Asia">Asia</option>
+            <option value="Europe">Europa</option>
+            <option value="North America">North America</option>
+            <option value="Oceania">Oceania</option>
+            <option value="South America">South America</option>
+          </select>
+        </div>
+        <div className="div">
+          <label> Activity: </label>
+          <select
+            className="selectStyle"
+            onChange={(e) => handleFilterActivity(e)}
+          >
+            <option value="All"></option>
+            {allActivities.map((element) => (
+              <option value={element.name} key={element}>
+                {element.name}
+              </option>
+            ))}
+          </select>
+        </div>
       </div>
       <div className="cards">
         {currentCountries?.map((el) => {
           return (
             <div className="card">
-              <Link to={"/home/" + el.id} style={{ textDecoration: "none", color: "white" }}>
+              <Link
+                to={"/home/" + el.id}
+                style={{ textDecoration: "none", color: "white" }}
+              >
                 <Card
                   image={el.image}
                   name={el.name}
-                  id= {el.id}
+                  id={el.id}
                   continent={el.continent}
-                /> 
+                />
               </Link>
             </div>
           );
